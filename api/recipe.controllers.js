1b9202fa6c36cffb3a0b1f5072443ff7b43f59a1
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-04-16 19:32:27 -0400

    Created an Import and create a  Delete command (Kill ALL)

--- api/recipe.controllers.js
+++ api/recipe.controllers.js
@@ -9,6 +9,48 @@ exports.add = function () {};
 exports.update = function () {};
 exports.delete = function () {};
 
+// Example to delete just one Item
+// exports.killall = function (req, res) {
+//   Recipe.deleteMany({ title: "Lasagna" }).then(res.sendStatus(202));
+// };
+
+// Delete all recipies
+exports.killall = function (req, res) {
+  Recipe.deleteMany({}).then(res.sendStatus(202));
+};
+
+//Creates some default Data!
+exports.import = function (res) {
+  Recipe.create(
+    {
+      title: "Lasagna",
+      description:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+      image: "lasagna.png",
+    },
+    {
+      title: "Pho-Chicken Noodle Soup",
+      description:
+        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
+      image: "pho.png",
+    },
+    {
+      title: "Guacamole",
+      description:
+        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
+      image: "guacamole.png",
+    },
+    {
+      title: "Hamburger",
+      description:
+        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
+      image: "hamburger.png",
+    }
+  );
+};
+
+//.then(res.sendStatus(202));
+
 //Creating some data
 // app.get("/api/import", function (req, res) {
 //   Recipe.create(
--- server.js
+++ server.js
@@ -8,16 +8,19 @@ app.use(express.static("static"));
 
 // Route - index.html file
 //note (req, res, next) format
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/static/index.html");
-});
-
 //Routes  These represent the CRUD operations
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/static/index.html"); //__dirname is special function that gives the directory of the server location where this is running
+});
 app.get("/api/recipes", recipeControllers.findAll);
 app.get("/api/recipes/:id", recipeControllers.findById);
 app.post("/api/recipes", recipeControllers.add);
 app.put("/api/recipes/:id", recipeControllers.update);
 app.delete("/api/recipes/:id", recipeControllers.delete);
+
+app.get("/api/import", recipeControllers.import);
+app.get("/api/killall", recipeControllers.killall);
+// app.post("/api/upload", recipeControllers.upload);
 
 // .connect(dataBaseURL, { useNewUrlParser: true })  deprecated option no longer needed
 mongoose
